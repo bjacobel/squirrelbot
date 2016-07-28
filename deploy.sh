fd2bf7731eb6bacf9f8191f759fdd4285de26502
Brian Jacobel <bjacobel@gmail.com>
2016-07-28 12:59:21 -0400

    Amend deploy script to make sure Lambda has permissions to be execed by SES

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 pushd dist
 
 zip -r function.zip ./*
@@ -5,5 +7,14 @@ zip -r function.zip ./*
 aws --profile=bjacobel lambda update-function-code \
   --function-name github-slack-notifier \
   --zip-file fileb://`pwd`/function.zip
+
+aws --profile=bjacobel lambda add-permission \
+  --function-name github-slack-notifier \
+  --action "lambda:InvokeFunction" \
+  --principal "ses.amazonaws.com" \
+  --source-account "956518986395" \
+  --statement-id "GiveSESPermissionToInvokeFunction" \
+  2>/dev/null 1>/dev/null
+
 
 popd dist
