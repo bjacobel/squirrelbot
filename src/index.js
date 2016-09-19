aec8244a588b22a117643c441365cda992ee7717
Brian Jacobel <bjacobel@gmail.com>
2016-09-18 21:08:28 -0400

    Add parsing for links and images

--- src/index.js
+++ src/index.js
@@ -3,6 +3,68 @@ import HTMLParser from 'fast-html-parser';
 
 import { post } from './slack';
 
+const parseBody = (lines) => {
+  return `${lines.slice(0, -4).join('\n')}\n`;
+};
+
+const parseReply = (lines) => {
+  const footer = lines.slice(lines.length - 4, lines.length).join('\n');
+
+  return footer.match(
+    /Reply to this email directly or view it on GitHub:\r\n(https:\/\/github.com\/.*)/
+  )[1];
+};
+
+const parseCode = (messageHTML, userMessage) => {
+  const root = HTMLParser.parse(messageHTML, { pre: true });
+  let messageWithBacktickedCode = userMessage;
+  const codeBlocks = root.querySelectorAll('pre');
+
+  codeBlocks.forEach((codeBlock) => {
+    if (codeBlock.text.startsWith('<code>')) {
+      // Skip these, user-supplied code blocks will already be backticked in the message plaintext
+      return;
+    } else {
+      messageWithBacktickedCode = messageWithBacktickedCode.replace(
+        codeBlock.text,
+        `\`\`\`\r\n${codeBlock.text}\`\`\`\r\n`
+      );
+    }
+  });
+
+  return messageWithBacktickedCode;
+};
+
+const parseImages = (userMessage) => {
+  let parsedForImages = userMessage;
+
+  // Match inline images
+  const inlineRegexp = new RegExp(/!\[(.*)\]\((.*)\)/g);
+  let match = inlineRegexp.exec(parsedForImages);
+
+  while (match) {
+    parsedForImages = parsedForImages.replace(match[0], match[2]);
+    match = inlineRegexp.exec(parsedForImages);
+  }
+
+  return parsedForImages;
+};
+
+const parseLinks = (userMessage) => {
+  let parsedForLinks = userMessage;
+
+  // Match links to images
+  const linkRegexp = new RegExp(/\[(.*)\]\((.*)\)/g);
+  let match = linkRegexp.exec(parsedForLinks);
+
+  while (match) {
+    parsedForLinks = parsedForLinks.replace(match[0], `<${match[2]}|${match[1]}>`);
+    match = linkRegexp.exec(parsedForLinks);
+  }
+
+  return parsedForLinks;
+};
+
 export const handler = (event, context, callback, skipParse) => {  // eslint-disable-line import/prefer-default-export
   try {
     let jsonEvent;
@@ -14,25 +76,13 @@ export const handler = (event, context, callback, skipParse) => {  // eslint-dis
 
     const message = jsonEvent['body-plain'];
     const messageHTML = jsonEvent['body-html'];
-
     const lines = message.split('\n');
-    let userMessage = `${lines.slice(0, -4).join('\n')}\n`;
-    const footer = lines.slice(lines.length - 4, lines.length).join('\n');
 
-    const replyLink = footer.match(
-      /Reply to this email directly or view it on GitHub:\r\n(https:\/\/github.com\/.*)/
-    )[1];
-
-    const root = HTMLParser.parse(messageHTML, { pre: true });
-    const codeBlocks = root.querySelectorAll('pre');
-    codeBlocks.forEach((codeBlock) => {
-      if (codeBlock.text.startsWith('<code>')) {
-        // Skip these, user-supplied code blocks will already be backticked in the message plaintext
-        return;
-      } else {
-        userMessage = userMessage.replace(codeBlock.text, `\`\`\`\r\n${codeBlock.text}\`\`\`\r\n`);
-      }
-    });
+    let userMessage = parseBody(lines);
+    const replyLink = parseReply(lines);
+    userMessage = parseCode(messageHTML, userMessage);
+    userMessage = parseImages(userMessage);
+    userMessage = parseLinks(userMessage);
 
     post({
       subject: jsonEvent.subject,
